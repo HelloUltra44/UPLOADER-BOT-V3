3edb5395e1f3079255d07ba37ecc7152d298db11
HelloUltra44 <122900262+HelloUltra44@users.noreply.github.com>
2023-01-29 11:41:24 +0530

    Update config.py

--- plugins/config.py
+++ plugins/config.py
@@ -11,7 +11,7 @@ logging.basicConfig(
 
 class Config(object):
 
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5896381925:AAEhvUfE7QTElvyi_00Geagz4Wttw_OC908")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5570243067:AAEI_RnFlFvTlbkSQYc_fA0KHDXl4twOgG8")
 
     API_ID = int(os.environ.get("API_ID", 23560088))
 
@@ -37,17 +37,17 @@ class Config(object):
 
     HTTP_PROXY = os.environ.get("HTTP_PROXY", "")
 
-    OUO_IO_API_KEY = ""
+    OUO_IO_API_KEY = "kLgE2EQm"
 
     MAX_MESSAGE_LENGTH = 4096
 
     PROCESS_MAX_TIMEOUT = 0
 
-    DEF_WATER_MARK_FILE = "Use this bot @UploadLinkToFileBot"
+    DEF_WATER_MARK_FILE = "Use this bot @ulruploaderv2bot"
 
     DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://amit:amit@cluster0.fdlnmlh.mongodb.net/?retryWrites=true&w=majority")
 
-    SESSION_NAME = os.environ.get("SESSION_NAME", "UploadLinkToFileBot")
+    SESSION_NAME = os.environ.get("SESSION_NAME", "ulruploaderv2bot")
 
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001642382009"))
 
@@ -56,6 +56,6 @@ class Config(object):
     OWNER_ID = int(os.environ.get("OWNER_ID", "5410723702"))
 
 
-    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
+    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001666254490")
     
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "UploadLinkToFileBot")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "ulruploaderv2bot")
